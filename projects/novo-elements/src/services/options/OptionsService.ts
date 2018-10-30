78edf72e67ae5a035deace614be6e1153ae81449
John Sullivan <john.sullivan@bullhorn.com>
2018-10-30 13:17:17 -0400

    feat(secondaryFilters) passing secondaryFilter to optionsUrl if present

--- projects/novo-elements/src/services/options/OptionsService.ts
+++ projects/novo-elements/src/services/options/OptionsService.ts
@@ -8,13 +8,16 @@ export class OptionsService {
   constructor() {}
 
   getOptionsConfig(http: HttpClient, field: any, config: { token?: string; restUrl?: string; military?: boolean }): any {
+    const secondaryFilterParam =
+      field.secondaryFilter && field.secondaryFilter.length > 0 ? `&secondaryFilter=${field.secondaryFilter}` : '';
+
     return {
       field: 'value',
       format: '$label',
       options: (query) => {
         return new Promise((resolve, reject) => {
           if (query && query.length) {
-            http.get(`${field.optionsUrl}?filter=${query || ''}`).subscribe(resolve, reject);
+            http.get(`${field.optionsUrl}?filter=${query || ''}${secondaryFilterParam}`).subscribe(resolve, reject);
           } else {
             resolve([]);
           }
