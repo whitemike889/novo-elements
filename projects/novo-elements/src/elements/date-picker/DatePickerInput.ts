975af19b02649bbd300ee9180792602e10e67a96
Ezra Shim <ezra.shim@bullhorn.com>
2019-03-06 21:02:01 -0500

    add BBO Euro userDefinedFormat

--- projects/novo-elements/src/elements/date-picker/DatePickerInput.ts
+++ projects/novo-elements/src/elements/date-picker/DatePickerInput.ts
@@ -37,12 +37,30 @@ const DATE_VALUE_ACCESSOR = {
   selector: 'novo-date-picker-input',
   providers: [DATE_VALUE_ACCESSOR],
   template: `
-        <input type="text" [name]="name" [(ngModel)]="formattedValue" [textMask]="maskOptions" [placeholder]="placeholder" (focus)="_handleFocus($event)" (keydown)="_handleKeydown($event)" (input)="_handleInput($event)" (blur)="_handleBlur($event)" #input data-automation-id="date-input" [disabled]="disabled"/>
-        <i *ngIf="!hasValue" (click)="openPanel()" class="bhi-calendar"></i>
-        <i *ngIf="hasValue" (click)="clearValue()" class="bhi-times"></i>
-        <novo-overlay-template [parent]="element" position="above-below">
-            <novo-date-picker [start]="start" [end]="end" inline="true" (onSelect)="setValueAndClose($event)" [ngModel]="value"></novo-date-picker>
-        </novo-overlay-template>
+    <input
+      type="text"
+      [name]="name"
+      [(ngModel)]="formattedValue"
+      [textMask]="maskOptions"
+      [placeholder]="placeholder"
+      (focus)="_handleFocus($event)"
+      (keydown)="_handleKeydown($event)"
+      (input)="_handleInput($event)"
+      (blur)="_handleBlur($event)"
+      #input
+      data-automation-id="date-input"
+      [disabled]="disabled"
+    />
+    <i *ngIf="!hasValue" (click)="openPanel()" class="bhi-calendar"></i> <i *ngIf="hasValue" (click)="clearValue()" class="bhi-times"></i>
+    <novo-overlay-template [parent]="element" position="above-below">
+      <novo-date-picker
+        [start]="start"
+        [end]="end"
+        inline="true"
+        (onSelect)="setValueAndClose($event)"
+        [ngModel]="value"
+      ></novo-date-picker>
+    </novo-overlay-template>
   `,
 })
 export class NovoDatePickerInputElement implements OnInit, ControlValueAccessor {
@@ -93,7 +111,7 @@ export class NovoDatePickerInputElement implements OnInit, ControlValueAccessor 
   }
 
   ngOnInit() {
-    this.userDefinedFormat = this.format ? !this.format.match(/^(DD\/MM\/YYYY|MM\/DD\/YYYY)$/g) : false;
+    this.userDefinedFormat = this.format ? !this.format.match(/^(DD\/MM\/YYYY|MM\/DD\/YYYY|DD\-MM\-YYYY)$/g) : false;
     if (!this.userDefinedFormat && this.textMaskEnabled && !this.allowInvalidDate) {
       this.maskOptions = this.maskOptions || {
         mask: [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
